af4f329b8e3d7e0597eb3c4d3df27bbf76a51c56
박정호 <qkrwjdwjdghgh13@naver.com>
2022-02-19 16:54:51 +0900

    a

--- devel/share/drone_system/cmake/drone_system-msg-extras.cmake
+++ devel/share/drone_system/cmake/drone_system-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(drone_system_MESSAGE_FILES "/home/lsy/drone_ws/src/drone_system/msg/Status.msg")
+set(drone_system_MESSAGE_FILES "/home/parkjh/mavsdktest/MAVSDK-Python/drone/drone_ws/src/drone_system/msg/Status.msg")
 set(drone_system_SERVICE_FILES "")
